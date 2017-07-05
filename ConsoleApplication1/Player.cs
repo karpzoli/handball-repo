2235565a6a31b2927a060a4540ced49dc39c5dae
tanulo8@athenepraxis.local <tanulo8@athenepraxis.local>
2017-07-05 18:31:06 +0200

    Új Classok

---
+++ ConsoleApplication1/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Player
+    {
+        public int Number { get; set; }
+        public bool Court { get; set; }
+        public int Minits { get; set; }
+    }
+}
---
+++ ConsoleApplication1/Sub.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Sub
+    {
+        public int min { get; set; }
+        public int Who { get; set; }
+        public int Relase { get; set; }
+    }
+}
