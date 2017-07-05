8f7a7f54a7056e2229de2673dc8d92e0291f28be
kelemenlajos@athenepraxis.local <kelemenlajos@athenepraxis.local>
2017-07-05 20:46:23 +0200

    startmenü

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -3,13 +3,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace ConsoleApplication1
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            {
+                StartMenu();
+            }
+        }
+        static void StartMenu()
+        {
+            string menu = null;
+            var player = FileHandler.ReadPlayers();
+            var subs = FileHandler.ReadSubs();
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("***Kézilabda szimuláció***");
+                Console.Write("1: :szimuláció futtatása\n\n2:kilépés\nParancs: ");
+                menu = Console.ReadLine();
+                switch (menu)
+                {
+                    case "1":
+                        Console.Clear();
+                        var match = new Match(player, subs);
+                        match.PlayerMatch();
+                        //Display(Player[] players);
+
+                        Console.WriteLine("\n\n\nÜss le egy billentyűt a kilépéshez...");
+                        Console.ReadKey();
+                        break;
+                   
+                    case "2":
+                        break;
+                    default:
+                        Console.WriteLine("Rossz parancs...");
+                        Thread.Sleep(500);
+                        break;
+                }
+
+
+            } while (menu != "2");
+
         }
     }
 }
